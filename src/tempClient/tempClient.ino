ce3c69a250133dfd23f26bc8a656fe01ca89c8b4
Toto <torsten.quast1@gmail.com>
2017-05-09 16:27:11 +0200

    Switch and Display timeout
    Display has background light only while the switch is pressed or the next 3 seconds after that

--- src/tempClient/tempClient.ino
+++ src/tempClient/tempClient.ino
@@ -1,19 +1,27 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "DHT.h"
 
+#define PIN_DHT                  D3
+#define PIN_SWITCH               D0
+
+
+//DHT11 can only read every 2 s.
+#define INTERVAL_DHT_READ      2000
+#define INTERVAL_DISPLAY_DIM   3000
+
+DHT dht(PIN_DHT, DHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-#include "DHT.h"
-DHT dht(D3, DHT11);
-
-uint32_t lastReadDht;
+uint32_t timeSwitchReleased;
+uint32_t timeDhtRead;
 int temp;
 int humi;
+bool switchStateOld;
 
 void readDht() {
-  if ((lastReadDht + 2000) <= millis()) {
-    //DHT11 can only read every 2 s.
-    lastReadDht = millis();
+  if ((timeDhtRead + INTERVAL_DHT_READ) <= millis()) {
+    timeDhtRead = millis();
     if (dht.read()) {
       humi = dht.readHumidity();
       temp = dht.readTemperature();
@@ -21,7 +29,20 @@ void readDht() {
   }
 }
 
-void writeToDisplay() {
+void updateDisplay() {
+  bool isSwitchPressed = digitalRead(PIN_SWITCH) == HIGH;
+
+  if (isSwitchPressed) {
+    timeSwitchReleased = millis();
+  }
+
+  if (isSwitchPressed || timeSwitchReleased + INTERVAL_DISPLAY_DIM > millis()) {
+    lcd.backlight();
+  }
+  else {
+    lcd.noBacklight();
+  }
+
   lcd.setCursor(0, 0);
   lcd.print(temp);
   lcd.print(" ");
@@ -37,10 +58,12 @@ void setup()
 {
   lcd.begin();
   dht.begin();
+  pinMode(D0, INPUT);
 }
 
 void loop()
 {
   readDht();
-  writeToDisplay();
+  updateDisplay();
+  delay(100);
 }
